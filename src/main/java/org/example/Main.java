03946ba025bfb47ff4616cde8cc950d39483c791
mLUFBz8khYPUVjK <jdvillamil@eservicios.indra.es>
2024-09-19 18:21:39 +0200

    Refactor code and change List for Stacks - Vers 1.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -7,133 +9,98 @@ public class Main {
 
     public static Set<Integer> rowBlock = new HashSet<>();
     public static Set<Integer> colBlock = new HashSet<>();
-    public static Set<Double> diagUDBlock = new HashSet<>();
-    public static Set<Double> diagDUBlock = new HashSet<>();
-    public static Set<Double> queens = new HashSet<>();
-    public static Set<Set<Double>> queenstEST = new HashSet<>();
+    public static Set<String> diagUDBlock = new HashSet<>();
+    public static Set<String> diagDUBlock = new HashSet<>();
+    public static Stack<String> queens = new Stack<>();
+    public static Stack<String> queensNoSolution = new Stack<>();
 
     public static void main(String[] args) {
         System.out.print("Número de filas/columnas/reinas: ");
         Scanner scan = new Scanner(System.in);
         final int quantity = scan.nextInt();
 
-        final int[][] matrix = new int[quantity][quantity];
+        Instant start = Instant.now();
 
-        int locatedQueens = 0;
+        executeCalculates(0, quantity, queens);
 
-        printTable(new HashSet<>(), matrix);
-
-        
-        for (int i = 0; i < quantity; i++) {
-            executeCalculates(i, quantity, queens);
+        if (queens.size() == quantity) {
             System.out.println("************************************************");
-            System.out.println("*************** \t\t" + i + "\t\t****************");
             System.out.println("************************************************");
-
-            if (queens.size() == quantity) {
-                System.out.println("************************************************");
-                System.out.println("************************************************");
-                System.out.println("************************************************");
-                System.out.println("************************************************");
-                System.out.println("************************************************");
-
-                System.out.println("************************************************");
-                System.out.println("*************** ENCONTRADO    ******************");
-                System.out.println("************************************************");
-                break;
-            }
-            resetSets();
-            queens.clear();
+            System.out.println("*************** ENCONTRADO    ******************");
+            System.out.println("************************************************");
         }
+
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println("Duración: " + timeElapsed.toMillis() + " milisegundos");
 
     }
-/*
-    public static boolean test (int row, int col, int quantity) {
-        Double coord = convertTwoIntToDouble(row, col);
-        if (queens.size() == quantity)
-            return true;
-        if (queens.size() < quantity)
-            return false;
-        if (!isValid(row, col, coord)) {
-            //resetSets();
-            test(row, col+1, quantity);
-            col = 0;
-        } else {
-            queens.add(coord);
-            save(queens, row, col, coord, quantity);
-            return test(row+1, col, quantity);
-        }
-    }*/
-    private static void executeCalculates(int numIter, int quantity, Set<Double> queens) {
-        //if (numIter >= quantity)
-          //  return;
+
+    private static void executeCalculates(int numIter, int quantity, Stack<String> queens) {
         int numIterInit = numIter;
-        int cont = 0;
-        for (int row = 0; row < quantity; row ++) {
+        int flag = 1;
+
+        for (int row = 0; row < quantity; row = row + flag) {
+            int posibilitiesFound = 0;
             for (int col = numIter; col < quantity; col ++ ) {
 
-                Double coord = convertTwoIntToDouble(row, col);
-                cont++;
-                //if (!isValid(row, col, coord)) {
-                if (!validate(row, col, quantity, queens)) {
-                     //resetSets();
-                     continue;
+                //Double coord = convertTwoIntToDouble(row, col);
+                String coord = row + "." + col;
+                if (validate(row, col, quantity, queens)) {
+                    queens.push(coord);
+                    posibilitiesFound++;
+                    break;
                 }
-                queens.add(coord);
-                if (queenstEST.contains(queens))
-                    queens.remove(coord);
-                 //save(queens, row, col, coord, quantity);
             }
-            queenstEST.add(Set.copyOf(queens));
-            numIter = 0;
-            cont=0;
+            printTable(queens, new int[quantity][quantity]);
             if (queens.size() < row+1) {
-                printTable(queens, new int[quantity][quantity]);
-                resetSets();
-                queens.clear();
-                return;
-            }
-        }
-        if (queens.size() != quantity) {
-            if (!queens.isEmpty()) {
-                List<Double> list = queens.stream().collect(Collectors.toList());
-                diagDUBlock.add(list.get(list.size() - 1));
-                queens.remove(list.get(list.size() - 1));
+                cleanStackOneRow(queensNoSolution, row+1);
+                if (row-1 == 0) {
+                    cleanStack(queensNoSolution);//queensNoSolution.clear();
+                }
+
+                if (validateEndRow(queens.peek(), quantity)) {
+                    cleanStackOneRow(queensNoSolution, row-1);
+                    cleanStackOneRow(queensNoSolution, row);
+                    queens.pop();
+                    flag = -2;
+                } else {
+                    flag = -1;
+                }
+                queensNoSolution.push(queens.pop());
+
+                if (row-1 <= -1) {
+                    return;
+                }
             } else {
-                resetSets();
-                queens.clear();
+                flag = 1;
             }
-            //executeCalculates(numIterInit +1, quantity, queens);
+            numIter = 0;
         }
+
         printTable(queens, new int[quantity][quantity]);
     }
 
-    public static boolean validate(int row, int col, int quantity, Set<Double> queens) {
-        if (queens.isEmpty())
+    public static boolean validate(int row, int col, int quantity, Stack<String> queens) {
+        if (queens.isEmpty() && queensNoSolution.isEmpty())
             return true;
         queens.forEach(coord -> {
             String[] arr = String.valueOf(coord).split("\\.");
             int rowCoord = Integer.parseInt(arr[0]);
             int colCoord = Integer.parseInt(arr[1]);
-            //save(queens, rowCoord, colCoord, coord, quantity);
-            rowBlock.add(rowCoord);
-            colBlock.add(colCoord);
-            diagUDBefore(rowCoord, colCoord);
-            diagUDAfter(rowCoord, colCoord, quantity);
-            diagDUBefore(rowCoord, colCoord, quantity);
-            diagDUAfter(rowCoord, colCoord, quantity);
+            chargeRestrictions(rowCoord, colCoord, quantity);
         });
-        boolean result = isValid(row, col, convertTwoIntToDouble(row,col));
+        boolean result = isValid(row, col, row +"."+ col);
         resetSets();
         return result;
     }
 
-    public static boolean isValid(int rowNumber, int colNumber, double coord) {
+    public static boolean isValid(int rowNumber, int colNumber, String coord) {
         if (rowBlock.contains(rowNumber)) return false;
         if (colBlock.contains(colNumber)) return false;
         if (diagUDBlock.contains(coord)) return false;
         if (diagDUBlock.contains(coord)) return false;
-
+        if (queensNoSolution.contains(coord)) return false;
         return true;
     }
 
@@ -144,26 +111,23 @@ public class Main {
         diagDUBlock.clear();
     }
 
-    public static void save(Set<Double> coordQueens, int row, int col, double coord, int quantity) {
+    public static void chargeRestrictions(int row, int col, int quantity) {
         rowBlock.add(row);
         colBlock.add(col);
         diagUDBefore(row, col);
         diagUDAfter(row, col, quantity);
         diagDUBefore(row, col, quantity);
         diagDUAfter(row, col, quantity);
-
-        //resetSets();
-        printTable(coordQueens, new int[quantity][quantity]);
     }
 
 
-    public static void printTable(Set<Double> coordQueens, int[][] matrix) {
+    public static void printTable(Stack<String> coordQueens, int[][] matrix) {
         char c = '_';
         String separator = "\n" + String.valueOf(c).repeat(4).repeat(matrix.length);
         System.out.println(separator);
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix.length; col++) {
-                if (!coordQueens.isEmpty() && coordQueens.contains(convertTwoIntToDouble(row, col))) {
+                if (!coordQueens.isEmpty() && coordQueens.contains(row+"."+ col)) {
                     System.out.print("R" + "\t|");
                 } else {
                     System.out.print(matrix[row][col] + "\t|");
@@ -171,6 +135,7 @@ public class Main {
             }
             System.out.println(separator);
         }
+        System.out.println("\n*****************************\n");
     }
 
     public static double convertTwoIntToDouble(int num1, int num2) {
@@ -180,7 +145,7 @@ public class Main {
     public static void diagUDBefore(int row, int col) {
         if (row <= -1 || col <= -1)
             return;
-        diagUDBlock.add(convertTwoIntToDouble(row,col));
+        diagUDBlock.add(row+"."+col);//convertTwoIntToDouble(row,col));
 
         diagUDBefore(row-1, col-1);
     }
@@ -188,7 +153,7 @@ public class Main {
     public static void diagUDAfter(int row, int col, int quantity) {
         if (row >= quantity || col >= quantity)
             return;
-        diagUDBlock.add(convertTwoIntToDouble(row,col));
+        diagUDBlock.add(row+"."+col);//convertTwoIntToDouble(row,col));
 
         diagUDAfter(row+1, col+1, quantity);
     }
@@ -196,7 +161,7 @@ public class Main {
     public static void diagDUBefore(int row, int col, int quantity) {
         if (row >= quantity || col <= -1)
             return;
-        diagDUBlock.add(convertTwoIntToDouble(row,col));
+        diagDUBlock.add(row+"."+col);//convertTwoIntToDouble(row,col));
 
         diagDUBefore(row+1, col-1, quantity);
     }
@@ -204,8 +169,25 @@ public class Main {
     public static void diagDUAfter(int row, int col, int quantity) {
         if (row <= -1 || col >= quantity)
             return;
-        diagDUBlock.add(convertTwoIntToDouble(row,col));
+        diagDUBlock.add(row+"."+col);//convertTwoIntToDouble(row,col));
 
         diagDUAfter(row-1, col+1, quantity);
+    }
+
+    public static void cleanStack(Stack<String> stack) {
+        List<String> list = stack.stream().filter(item -> Integer.valueOf(item) < 1).collect(Collectors.toList());
+        stack.clear();
+        list.forEach(item -> stack.push(item));
+    }
+
+    public static void cleanStackOneRow(Stack<String> stack, int row) {
+        List<String> list = stack.stream().filter(item -> Double.valueOf(item) < row || Double.valueOf(item) >= row+1).collect(Collectors.toList());
+        stack.clear();
+        list.forEach(item -> stack.push(item));
+    }
+
+    public static boolean validateEndRow(String coord, int quantity) {
+        final String[] a2rr = coord.split("\\.");
+        return Integer.valueOf(a2rr[1]).intValue() == (quantity-1) ? true : false;
     }
 }
